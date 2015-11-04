d7cde42e7ad3c0453531d02cbcae1b6c29542f8b
Alex Vig <jalexvig@gmail.com>
2015-11-03 20:04:07 -0500

    Added multiprocess

---
+++ multiprocess.py
@@ -0,0 +1,28 @@
+import multiprocessing as mp
+
+def g(y):
+    for x in range(y):
+        print('gen ', x)
+        yield x
+
+def myfn(x):
+    print('fn ', x)
+    return x
+
+if __name__ == '__main__':
+
+    pool = mp.Pool(processes=2)
+
+    # figure out if imap is effective
+    # res = pool.imap(myfn, g(15))
+    # input('pause')
+    # for x in res:
+    #     input('res %d' % x)
+
+    pool.close()
+    pool.join()
+
+    res = map(myfn, g(15))
+    input('pause')
+    for x in res:
+        input('res %d' % x)
