df15f6e248d32883cd8aa4a871dd3d9bc25ae88e
Alex Vig <jalexvig@gmail.com>
2018-06-04 13:52:35 -0400

    Corrected multiprocessing

--- multiprocess_lazy_load.py
+++ multiprocess_lazy_load.py
@@ -31,6 +31,7 @@ def apply_async_ll(fn, iterable, nprocs=2):
     results = []
 
     consumed = True
+    num_in, num_out = 0, 0
     while True:
 
         try:
@@ -38,15 +39,26 @@ def apply_async_ll(fn, iterable, nprocs=2):
                 x = next(it)
                 consumed = False
             jobs_q.put(x, block=False)
-            consumed = True
         except queue.Full:
             res = results_q.get(block=True)
             results.append(res)
+            num_out += 1
         except StopIteration:
             break
+        else:
+            num_in += 1
+            consumed = True
 
     for _ in range(nprocs):
         jobs_q.put(None, block=True)
+
+    # Need this bit since the process might not exit if there is still data left in the results_q. When objects in the
+    # queue are large they are buffered. The process won't exit until the buffer is flushed
+    # https://stackoverflow.com/questions/26025486/python-processes-not-joining
+    while num_out < num_in:
+        res = results_q.get(block=True)
+        results.append(res)
+        num_out += 1
 
     for proc in procs:
         proc.join()
